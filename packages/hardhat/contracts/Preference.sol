106c2fe079e5bcaf0f478ae88d398e97b08d39e6
Kenil shah <kenilshah1505@gmail.com>
2023-09-27 04:22:29 +0530

    added one frrontend page

--- packages/hardhat/contracts/Preference.sol
+++ packages/hardhat/contracts/Preference.sol
@@ -1,18 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.18;
 
 contract Preference  {
  
     address primary;
     struct User{
-        address[] secondary;
+        address primaryAddress;
+        address[] secondaryAddresses;
         string chainPreference;
         string tokenPreference;
     }
     mapping(address => User) public users;
     event UserRegistered(address indexed userAddress);
      function registerUser(
-        address primary
+        address  _primaryAddress,
         address[] memory _secondaryAddresses,
         string memory _chainPreference,
         string memory _tokenPreference
@@ -38,12 +39,26 @@ contract Preference  {
         )
     {
         User storage user = users[_userAddress];
+        
         return (
             user.primaryAddress,
             user.secondaryAddresses,
             user.chainPreference,
             user.tokenPreference
         );
+    }
+    function getPrimaryAddress(address secondary)  public view returns (address){
+         User storage user = users[msg.sender];
+            if (user.primaryAddress == secondary) {
+            return secondary; // The provided secondary address is the primary address
+        }
+
+        for (uint256 i = 0; i < user.secondaryAddresses.length; i++) {
+            if (user.secondaryAddresses[i] == secondary) {
+                return user.primaryAddress; // Return the primary address
+            }
+        }
+        revert("Secondary address not found");
     }
 
 }
